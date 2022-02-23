4e789d0f0e45fdf4c880b97e686abfa3db801140
MaK <muminkhan2000@gmail.com>
2022-02-23 14:57:27 +0530

    added jwt_token

--- Backend/index.js
+++ Backend/index.js
@@ -7,8 +7,9 @@ const port = 5000
 app.use(express.json())
 //API.s
 app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
+ app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
-
-router.post('/', 
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+const jwt_secret ="Khatmina@987"
+// Create a user. POST "api/auth/createuser" No login required
+router.post('/createuser', 
 body('name').isLength({ min: 2 }),
 body('email','Invalid-email').isEmail(),
 body('password').isLength({ min: 5 }),
@@ -20,12 +23,57 @@ async (req, res) => {
   if (user){
     return res.status(400).json({error : 'A user with this email already exists !'})
   } 
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPass = await bcrypt.hash(req.body.password,salt);
   user = await  User.create({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
-  }) 
-  res.json(user)
+    password: hashedPass,
+  })
+  
+  const  data = 
+    {user:{
+    id : user.id
+  }
+    }
+  const jwt_token = jwt.sign(data,jwt_secret)
+  res.json({jwt_token})
+
+} catch (error) {
+    console.error(error)
+    res.status(500).send('Error')
+}
+})
+// Login. POST "api/login" No login required
+router.post('/login', 
+body('email','Invalid-email').isEmail(),
+body('password').isLength({ min: 1 }),
+
+async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  } 
+  try {
+    
+ const {email,password} = req.body
+  let user = await User.findOne({email})
+  if (!user){
+    return res.status(400).json({error : 'Either email or/and password is incorrect'})
+  } 
+  const real_user = await bcrypt.compare(password,user.password)
+  if (!real_user){
+    return res.status(400).json({error : 'Either email or/and password is incorrect'})
+  } 
+  
+  const  data = 
+    {user:{
+    id : user.id
+  }
+    }
+  const jwt_token = jwt.sign(data,jwt_secret)
+  res.json({jwt_token})
 
 } catch (error) {
     console.error(error)
