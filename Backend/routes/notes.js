dfc0113cf635b5bdb85248eb9aaa1dfe333dd893
MaK <muminkhan2000@gmail.com>
2022-02-25 15:56:30 +0530

    added CRUD

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -18,34 +18,84 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
     try {
-      const { title, description,category } = req.body;
+      const { title, description, category } = req.body;
       const note = new Notes({
-        title, description, category, user: req.user.id
-    })
-    const savedNote = await note.save()
-    res.json(savedNote)
+        title,
+        description,
+        category,
+        user: req.user.id,
+      });
+      const savedNote = await note.save();
+      res.json(savedNote);
     } catch (error) {
       console.error(error);
       res.status(500).send("Error");
     }
   }
 );
-// Fetch Notes of a User. GET "api/notes/add-note" No login required
+// Fetch Notes of a User. GET "api/notes/add-note"  login required
 router.get(
   "/fetch-notes",
-  
+
   fetchuser,
   async (req, res) => {
-    const errors = validationResult(req);
-    
     try {
-  
-      const notes = await Notes.find({user:req.user.id})
-    res.json(notes)
+      const notes = await Notes.find({ user: req.user.id });
+      res.json(notes);
     } catch (error) {
       console.error(error);
       res.status(500).send("Error");
     }
   }
 );
+// Update Note of a User. PUT "api/notes/update-note/:id"  login required
+router.put("/update-note/:id", fetchuser, async (req, res) => {
+  try {
+    const { title, description, category } = req.body;
+    const newNote = {};
+    if (title) {
+      newNote.title = title;
+    }
+    if (description) {
+      newNote.description = description;
+    }
+    if (category) {
+      newNote.category = category;
+    }
+
+    let note = await Notes.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send("Not Found");
+    }
+    if (note.user.toString() !== req.user.id) {
+      {
+        return res.status(401).send("You don;t have the authourity");
+      }
+    }
+    note = await Notes.findByIdAndUpdate(req.params.id, newNote, { new: true });
+    res.json(note);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error");
+  }
+});
+// Delete Note of a User. PUT "api/notes/delete-note/:id"  login required
+router.delete("/delete-note/:id", fetchuser, async (req, res) => {
+  try {
+    let note = await Notes.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send("Not Found");
+    }
+    if (note.user.toString() !== req.user.id) {
+      {
+        return res.status(401).send("You don;t have the authourity");
+      }
+    }
+    note = await Notes.findByIdAndDelete(req.params.id);
+    res.json({ message: "successful", note: note });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error");
+  }
+});
 module.exports = router;
